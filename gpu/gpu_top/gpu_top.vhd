4590e033238d6c2339f37c6bea053a68021dddc1
SpiritasX <deja.bogovac@gmail.com>
2023-01-10 22:12:17 +0100

    Sorted and cleaned up files and directories

--- gpu/gpu_top/gpu_top.vhd
+++ gpu/gpu_top/gpu_top.vhd
@@ -10,10 +10,9 @@ entity gpu_top is
 	Port (
 		iCLK      : in std_logic;
 		inRST     : in std_logic;
-		iLOAD     : in std_logic_vector(N-1 downto 0);
-		iINSTR    : in std_logic_vector(47 downto 0);
 		i_pix_x   : in t_pix_x;
 		i_pix_y   : in t_pix_x;
+		iR        : in t_register_array;
 		o_pix_rgb :out t_rgb888
 	);
 end gpu_top;
@@ -21,7 +20,6 @@ end gpu_top;
 architecture Behavioral of gpu_top is
 
 	signal sCOMP : t_comp_array;
-	signal sR    : t_register_array;
 	signal sRCOLOR : INPUTS(N-1 downto 0);
 	
 	signal sCOMP_COMBINED : std_logic_vector(N-1 downto 0);
@@ -33,28 +31,17 @@ begin
 	G_1 : for i in 0 to N-1 generate
 		COMPARATOR : entity work.comparator
 		port map (
-			iA => sR(i)(39 downto 0),
+			iA => iR(i)(39 downto 0),
 			iB => i_pix_x,
 			iC => i_pix_y,
 			oQ => sCOMP(i)
 		);
 	end generate;
 	
-	G_2 : for i in 0 to N-1 generate
-		REGISTAR : entity work.registar
-		port map (
-			iCLK  => iCLK,
-			inRST => inRST,
-			iWE   => iLOAD(i),
-			iD    => iINSTR,
-			oQ    => sR(i)
-		);
-	end generate;
-	
 	G_3 : for i in 0 to N-1 generate
 		COLOR_CONVERTER : entity work.color_converter
 		port map (
-			i6bitColor  => sR(i)(45 downto 40),
+			i6bitColor  => iR(i)(45 downto 40),
 			o24bitColor => sRCOLOR(i)
 		);
 	end generate;
@@ -79,4 +66,5 @@ begin
 		iSEL => sCOLOR_SEL,
 		oQ   => o_pix_rgb
 	);
+
 end Behavioral;
